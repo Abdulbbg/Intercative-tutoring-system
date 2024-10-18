e159a307492ae611d7f09d79a17cb77dafc74ff3
Abdulrahman Abbas <abdouliabbas75@gmail.com>
2024-10-18 10:32:33 +0100

    Added domain to ALLOWED_HOSTS

--- backend/settings.py
+++ backend/settings.py
@@ -16,7 +16,12 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS = ['intercative-tutoring-system.onrender.com']
+ALLOWED_HOSTS = [
+    'localhost', 
+    '127.0.0.1', 
+    'interactive-tutoring-system.onrender.com'  # Add this domain
+]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
